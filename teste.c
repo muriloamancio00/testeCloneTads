dd0ad2085974a752adcd43f6df59d710c67c9ece
mikael1909 <kanto3210>
2020-02-14 10:12:00 -0300

    sei a veririfcar par ou impar

--- teste.c
+++ teste.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 
 int main(){
+    int a,b;
 
+    printf("digite um numero incrivel: ");
+    scanf("%d",&a);
+
+    if ( a % 2 != 0) {
+        printf("esse numero é IMPAR!!\n\n");
+    }
+    else {
+        printf("o tal de número é PAR\n\n");
+    }
+    
 
     return 0;
 }
